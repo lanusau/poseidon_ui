bae9b1443ba23986caae92dcdc50ecb06c24773f
Laimonas Anusauskas <lanusauskas@corp.untd.com>
2012-09-14 09:27:47 -0700

    Styling changes and moving main menu file

--- config/initializers/menu.rb
+++ config/initializers/menu.rb
@@ -1,5 +1,5 @@
 # Load top menu (for all modules) 
-PoseidonV3::Application.config.main_menu = YAML.load_file("#{Rails.root}/config/main_menu.yml")
+PoseidonV3::Application.config.main_menu = YAML.load_file(PoseidonV3::Application.config.main_menu_config_file)
 
 # Load and sub-menu (for this module only)
 PoseidonV3::Application.config.submenu = YAML.load_file("#{Rails.root}/config/submenu.yml")
