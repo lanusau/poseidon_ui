b7c451094dc2b7c47aaca4b5fa0f95f41547149e
Laimonas Anusauskas <lanusauskas@corp.untd.com>
2012-10-15 10:10:52 -0700

    Fixed typo

--- app/models/sso_survey.rb
+++ app/models/sso_survey.rb
@@ -2,6 +2,6 @@ class SsoSurvey < ActiveRecord::Base
   self.primary_key = 'survey_id'
   self.table_name = 'sso_survey'
 
-  has_many :sso_survey_supervisor_approvals, :foreign_key => "survey_supervisor_id", :dependent => :delete_all
+  has_many :sso_survey_supervisors, :foreign_key => "survey_id", :dependent => :delete_all
 
 end
