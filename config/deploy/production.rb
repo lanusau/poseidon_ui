a6fbac9984e26fee14355039d4e247706730c28b
Laimonas Anusauskas <lanusauskas@corp.untd.com>
2015-02-17 11:48:32 -0800

    Capistrano changes to make sure we correctly identify primary dbutil

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,14 @@
-server "dbutil01vgs.vgs.untd.com", :app, :web, :db, :primary => true
-server "dbutil01dca.dca.untd.com", :app, :web, :db, :primary => false
+require 'resolv'
+
+# Database migrations will only run on primary host
+@primary_host = Resolv.getname(Resolv.new.getaddress('dbutil.prod.untd.com'))
+
+# Set primary flag if server name matches primary server name
+def server_with_primary_flag(server_name)
+  server server_name, :app, :web, :db, :primary => (@primary_host == server_name)
+end
+
+server_with_primary_flag("dbutil01vgs.vgs.untd.com")
+server_with_primary_flag("dbutil01dca.dca.untd.com")
+
 set :branch, "production"
